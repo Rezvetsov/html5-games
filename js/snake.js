28b146e988e7ab446de0f50846c1629c4fe221c6
Artyom Rezvetsov <rezvetz@gmail.com>
2020-01-15 16:26:01 +0300

    Добавление змейки в список препятствий

--- js/snake.js
+++ js/snake.js
@@ -8,12 +8,13 @@ const directions = {
 class Snake {
     direction = directions.right;
     body = new Array();
+    appleEaten = 0;
     constructor() {
         this.body.push(new snakeCell(10, 10));
         this.body.push(new snakeCell(9, 10));
         this.body.push(new snakeCell(9,9));
         this.body.push(new snakeCell(9,8));
-        
+        obstacles.snake = this.snake;
     }
 
     /* Перемещение */
@@ -21,10 +22,10 @@ class Snake {
     // Удаляем хвост змейки (метод shrink)
     // Затем создаем на месте головы змейки новый блок,
     // который направлен в зависимости от полученного
-    // от пользователя направления (метод stretch)
+    // от пользователя направления (метод crawl)
     move(inputDirection) {
         this.shrink();
-        this.stretch(inputDirection);
+        this.crawl(inputDirection);
     }
     
     // Отрезать хвост для перемещения
@@ -33,7 +34,7 @@ class Snake {
     }
     
     // Добавить голову
-    stretch(inputDirection){
+    crawl(inputDirection){
         let prev = this.body[0];
 
         // UP 
