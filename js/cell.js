cf4b1fba46e573f55b808416eafdbdfb3d816566
Artyom Rezvetsov <rezvetz@gmail.com>
2020-01-15 11:42:58 +0300

    Удалены неисп. методы, добавлены позиции клеток

--- js/cell.js
+++ js/cell.js
@@ -1,4 +1,8 @@
 class Cell {
+    color;
+    
+    cellPositionX;
+    cellPositionY;
     positionX;
     positionY;
 
@@ -7,33 +11,12 @@ class Cell {
 
     indent = 1;
 
-    color = "#212121";
 
-    constructor(color) {
-        if (color != undefined)
-            this.color = color;
-    }
-    setPositionCell(cell) {
-        this.positionX = cell.positionX;
-        this.positionY = cell.positionY;
-    }
-    setPositionXY(posX, posY) {
-        if (posX == undefined) {
-            throw "Не задана позиция клетки по X";
-        }
-        if (posY == undefined) {
-            throw "Не задана позиция клетки по Y";
-        }
+    constructor(color, posX, posY) {
+        this.color = color;
+        this.cellPositionX = posX;
+        this.cellPositionY = posY;
         this.positionX = posX * (this.sizeX + this.indent);
         this.positionY = posY * (this.sizeY + this.indent);
-    }
-    
-    static cellToGlobalPosition(posX, posY){
-        let position = {
-            x : posX * (24 + 1),
-            y : posY * (24 + 1)
-        }
-
-        return position;
     }
 }
