fa460bd45f33bafed45993cf478aaa903066cb2a
Artyom Rezvetsov <rezvetz@gmail.com>
2020-01-17 13:38:32 +0300

    Добавлены границы игрового поля

--- js/grid.js
+++ js/grid.js
@@ -3,6 +3,7 @@ class Grid {
     sizeY = 24;
     cells = new Array();
     constructor() {
+        obstacles.setBorder(this.sizeX, this.sizeY);
         for (let i = 0; i < this.sizeX; i++) {
             this.cells[i] = new Array();
             for (let j = 0; j < this.sizeY; j++) {
--- js/obstacles.js
+++ js/obstacles.js
@@ -1,4 +1,11 @@
 class obstacles {
     static apple;
     static snake;
+
+    static border = {borderX: 0, borderY: 0};
+
+    static setBorder(x,y){
+        this.border.borderX = x;
+        this.border.borderY = y;
+    }
 }
