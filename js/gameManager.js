f9f400ff977bf2ed2e7ed816ed59ae602574485a
Artyom Rezvetsov <rezvetz@gmail.com>
2020-01-17 12:24:51 +0300

    Добавлен игровой цикл, пауза и геймовер

--- js/gameManager.js
+++ js/gameManager.js
@@ -1,12 +1,25 @@
 class GameManager {
+    interval = null;
+
     engine = {};
     grid = {};
     apple = {};
     snake = {};
+
+    direction = directions.right;
+    
+    isPause = true;
+    isGameOver = false;
+    
     constructor(canvas) {
         this.grid = new Grid();
         this.engine = new Engine(canvas, this.grid);
-            }
+
+        let body = document.getElementById('body');
+        body.addEventListener('keydown', function (e) {
+            gm.GetInput(e);
+        });
+    }
 
     Start() {
         this.engine.clear();
@@ -14,22 +27,48 @@ class GameManager {
         this.apple = new Apple(this.GetRandomCellPosition());
         this.engine.drawGrid(this.grid);
         this.engine.drawSnake(this.snake);
-        this.engine.drawApple(this.apple);        
+        this.engine.drawApple(this.apple);
+    }
+
+    startGame(){
+        if(this.isPause == false){
+            this.isPause = true;
+        } else{
+            // Если интервал == null, 
+            // то игра еще не начиналась
+            if (this.interval == null)
+                this.interval = setInterval("gm.NextFrame()", 100);
+
+            if(this.isGameOver == true){
+                this.Start();
+                this.isGameOver = false;
+            }
+            this.isPause = false;
+        }
+    }
+
+    gameOver(){
+        this.isPause = true;
+        this.isGameOver = true;
     }
 
     NextFrame(){
-        this.engine.clear();
-        this.engine.drawGrid(this.grid);
-        this.engine.drawSnake(this.snake);
+        console.log('here');     
+        if(this.isPause == false){
+            this.snake.move(this.direction);
+            this.engine.clear();
+            this.engine.drawGrid(this.grid);
+            this.engine.drawSnake(this.snake);
 
-        if(this.apple.eaten == true){
-            this.apple = new Apple(this.GetRandomCellPosition());
+            if (this.apple.eaten == true) {
+                this.apple = new Apple(this.GetRandomCellPosition());
+            }
+            this.engine.drawApple(this.apple);
+
+            if (this.snake.dead == true) {
+                this.gameOver();
+            }
         }
-        this.engine.drawApple(this.apple);
-
-        if(this.snake.dead == true){
-            this.Start();
-        }      
     }
 
     GetRandomCellPosition() {
@@ -39,27 +78,27 @@ class GameManager {
         return {x, y};
     }
 
-    GetInput(e) {  
+    GetInput(e) {        
+        if (e.keyCode == '32'){
+            this.startGame();
+        }
+
         /* Movement */      
         // Up
         if (e.keyCode == '87' || e.keyCode == '38'){
-            this.snake.move(directions.up);          
-            this.NextFrame();
+            this.direction = directions.up;     
         } 
         // Right
         else if (e.keyCode == '68' || e.keyCode == '39') {
-            this.snake.move(directions.right);
-            this.NextFrame();
+            this.direction = directions.right;     
         } 
         // Down
         else if (e.keyCode == '83' || e.keyCode == '40') {
-            this.snake.move(directions.down);
-            this.NextFrame();
+            this.direction = directions.down;     
         } 
         // Left
         else if (e.keyCode == '65' || e.keyCode == '37') {
-            this.snake.move(directions.left);
-            this.NextFrame();
+            this.direction = directions.left;     
         }
     }
 }
